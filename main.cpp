60c9805f27611a32e8c646848e4473c67a2821bf
shujanislam <shujanm.islam231@gmail.com>
2024-08-20 14:07:00 +0530

    added search-by-name feature

--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@ class Webpage {
     public:
         int index;
         string name;
+        string url;
         string content;
 
-        Webpage(int i, string n, string c) {
+        Webpage(int i, string n, string u, string c) {
             index = i;
             name = n;
+            url = u;
             content = c;
 
             cout << endl<< index << ". " << name << endl;
@@ -24,6 +26,7 @@ class Webpage {
 
         void printInfo() {
             cout <<endl << "Name: " << name << endl;
+            cout <<endl << "URL: " << url << endl;
             cout <<endl<< "Content: " << content << endl;
             for (int i = 0; i <= 40; i++) {
                 cout << "-";
@@ -38,9 +41,9 @@ int main() {
 
 	cout << "Available Webpages:" << endl;
     // Create webpage objects
-    Webpage w1(0, "Google", "Your Destination for all the web content");
-    Webpage w2(1, "Facebook", "Connect with your friends online");
-    Webpage w3(2, "YouTube", "Share videos with everyone");
+    Webpage w1(0, "Google", "https://google.com", "Your Destination for all the web content");
+    Webpage w2(1, "Facebook", "https://facebook.com", "Connect with your friends online");
+    Webpage w3(2, "YouTube", "https://youtube.com", "Share videos with everyone");
 
     // Array of pointers to Webpage objects
     Webpage* pages[] = { &w1, &w2, &w3 };
@@ -49,32 +52,66 @@ int main() {
     int arr[] = { w1.index, w2.index, w3.index };
     int len = sizeof(arr) / sizeof(arr[0]);
 
+    string arr_names[] = {w1.name, w2.name, w3.name};
+    int names_len = sizeof(arr_names)/sizeof(arr_names[0]);
+
     int index;
 
-    while (true) {
-        cout << "Enter the index to visit the website: " << endl;
-        cin >> index;
+    int choice;
 
-        // Check if the input is valid
-        if (cin.fail()) {
-            // Clear the error flag
-            cin.clear();
-            // Ignore invalid input in the buffer
-            cin.ignore(10000, '\n');
-            cout << "Invalid input. Please enter a valid integer." << endl;
+    cout<<"Choose: 1) Search by Index 2) Search by Name "<<endl;
+    cin>>choice;
+
+    if(choice == 1){
+        while (true) {
+            cout << "Enter the index to visit the website: " << endl;
+            cin >> index;
+
+            // Check if the input is valid
+            if (cin.fail()) {
+                // Clear the error flag
+                cin.clear();
+                // Ignore invalid input in the buffer
+                cin.ignore(10000, '\n');
+                cout << "Invalid input. Please enter a valid integer." << endl;
+            } else {
+                // Valid input received, exit the loop
+                break;
+            }
+        }
+
+        int pos = binarySearch(arr, len, index);
+
+        if (pos != -1) {
+            // Print info of the webpage found
+            pages[pos]->printInfo();
         } else {
-            // Valid input received, exit the loop
-            break;
+            cout << "Webpage not found!" << endl;
         }
     }
+    else if(choice == 2){
+        // code for linear search
+        string name;
+        cout << "Enter the name of the website: ";
+        cin.ignore(); // To clear the buffer
+        getline(cin, name); // To allow spaces in the name
 
-    int pos = binarySearch(arr, len, index);
-
-    if (pos != -1) {
-        // Print info of the webpage found
-        pages[pos]->printInfo();
-    } else {
-        cout << "Webpage not found!" << endl;
+        // Linear search logic
+        bool found = false;
+        for (int i = 0; i < len; i++) {
+            if (pages[i]->name == name) {
+                pages[i]->printInfo();
+                found = true;
+                break;
+            }
+        }
+        
+        if (!found) {
+            cout << "Webpage not found!" << endl;
+        }
+    }
+    else{
+        cout<<"Wrong Operator!"<<endl;
     }
 
     return 0;
